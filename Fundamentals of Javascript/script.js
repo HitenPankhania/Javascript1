21ea6c2f9c6fbb9457fbd94067cabc7ea0222e8c
hitenpankhania <pankhaniahiten4@gmail.com>
2023-08-08 10:29:22 +0530

    okie

--- Fundamentals of Javascript/script.js
+++ Fundamentals of Javascript/script.js
@@ -1,143 +1,168 @@
 
-let js ="Good";
-if (js === 'Good') 
-alert('Javascript is Fun');
+// let js ="Good";
+// if (js === 'Good') 
+// alert('Javascript is Fun');
 
-1+1+2+4-5
-console.log(1+1+2+4-5)
+// 1+1+2+4-5
+// console.log(1+1+2+4-5)
 
-let firstName="Hiten";
-let lastName="Pankhania";
-let y=27;
-console.log(firstName)
-console.log(lastName)
-console.log(y)
+// let firstName="Hiten";
+// let lastName="Pankhania";
+// let y=27;
+// console.log(firstName)
+// console.log(lastName)
+// console.log(y)
 
-let javaScriptisfun =true
-console.log(javaScriptisfun)
+// let javaScriptisfun =true
+// console.log(javaScriptisfun)
 
-console.log(typeof true);
-console.log(typeof "Hello");
-console.log(typeof 'Hello');
-console.log(typeof 100);
+// console.log(typeof true);
+// console.log(typeof "Hello");
+// console.log(typeof 'Hello');
+// console.log(typeof 100);
 
-javaScriptisfun = "yes"
-console.log(typeof javaScriptisfun);
-
-
-
-let month
-console.log(month);
-month = 1999
-console.log(month);
+// javaScriptisfun = "yes"
+// console.log(typeof javaScriptisfun);
 
 
 
-
-const birthYear=1999;
-console.log(birthYear)
-
-
-
- Mathematical Operators
-const now = 2023;
-const agejay =now-2001;
-const age hiten=now-1999;
-console.log(jay,hiten)
-
-console.log(hiten*2,jay/2,3**2)
+// let month
+// console.log(month);
+// month = 1999
+// console.log(month);
 
 
 
-const lastName= 'Pankhania';
-const firstName= 'Hiten';
-const middleName='Dinesh';
-console.log(firstName+" "+middleName+" " +lastName)
+
+// const birthYear=1999;
+// console.log(birthYear)
 
 
 
-Assignment Operators
-let x=20+10;
-x += 10;//x = x + 30
-x*=12; //x = x * 12
-x ++; //x = x + 1
-x --; //x = x - 1
-console.log(x)
+//  Mathematical Operators
+// const now = 2023;
+// const agejay =now-2001;
+// const age hiten=now-1999;
+// console.log(jay,hiten)
 
-Comparison Operators
- const now = 2023;
- const agejay =now-2001;
-const agehiten=now-1999;
-
- console.log(agejay > agehiten)
- console.log(agejay < agehiten)
- console.log(agejay >= 23)
-console.log(agehiten <= 24)
+// console.log(hiten*2,jay/2,3**2)
 
 
-Precedence is more for (  -  )
- const now = 2023;
- const agejay =now-2001;
- const agehiten=now-1999;
- console.log(now-1999 < now-2001)
- console.log(now-1999 > now-2001)
 
- let x , y ;
- x = y = 20 - 5 - 10 ; // x = y = 5 // x=5 , y=5 Both the x and y will have the same answer
- console.log(x,y)
-
- Average age (    )
-  const now = 2023;
- const agejay =now-2001;
-  const agehiten=now-1999;
-
-  const averageAge= (agejay + agehiten) / 2
-
-  console.log(agejay,agehiten,averageAge)
-
- const firstName = 'Hiten';
- const lastName  = 'Pankhania';
- const middleName = 'Dinesh';
- const birthYear = 1999;
- const presentYear =2023;
-
-console.log( 'I am'+' ' + firstName + ' ' + middleName  + ' '+ lastName + ' ')
+// const lastName= 'Pankhania';
+// const firstName= 'Hiten';
+// const middleName='Dinesh';
+// console.log(firstName+" "+middleName+" " +lastName)
 
 
-String Template
- const firstName = 'Hiten';
- const lastName  = 'Pankhania';
- const middleName = 'Dinesh';
- const birthYear = 1999;
- const presentYear =2023;
- const New = `I am ${firstName} ${middleName} ${lastName} Age ${presentYear - birthYear} `
- console.log(New)
 
-console.log(`Hello I am Hiten Pankhania Age 24 Currently Learining Javascript `)
+// Assignment Operators
+// let x=20+10;
+// x += 10;//x = x + 30
+// x*=12; //x = x * 12
+// x ++; //x = x + 1
+// x --; //x = x - 1
+// console.log(x)
 
-console.log(`Hello
-  You
-   There `)
+// Comparison Operators
+//  const now = 2023;
+//  const agejay =now-2001;
+// const agehiten=now-1999;
 
-console.log('Hello \n\ You \n\ There')  //Note NOT RECOM
-
-STOP STOP STOP STOP 
-
-const age =24;
-const isoldEnough = age >= 20
-if(isoldEnough) {
-    console.log('Is Leagle To Get Driving License')
-}
+//  console.log(agejay > agehiten)
+//  console.log(agejay < agehiten)
+//  console.log(agejay >= 23)
+// console.log(agehiten <= 24)
 
 
-const age = 15;
+// Precedence is more for (  -  )
+//  const now = 2023;
+//  const agejay =now-2001;
+//  const agehiten=now-1999;
+//  console.log(now-1999 < now-2001)
+//  console.log(now-1999 > now-2001)
 
-if(age=>18) {
-    console.log('Can get driving license')
-}else{
-    const yearsLeft = 18 -age;
-    console.log(`Cannot get driving license ${yearsLeft} Year:)`)
-}
+//  let x , y ;
+//  x = y = 20 - 5 - 10 ; // x = y = 5 // x=5 , y=5 Both the x and y will have the same answer
+//  console.log(x,y)
+
+//  Average age (    )
+//   const now = 2023;
+//  const agejay =now-2001;
+//   const agehiten=now-1999;
+
+//   const averageAge= (agejay + agehiten) / 2
+
+//   console.log(agejay,agehiten,averageAge)
+
+//  const firstName = 'Hiten';
+//  const lastName  = 'Pankhania';
+//  const middleName = 'Dinesh';
+//  const birthYear = 1999;
+//  const presentYear =2023;
+
+// console.log( 'I am'+' ' + firstName + ' ' + middleName  + ' '+ lastName + ' ')
+
+
+// String Template
+//  const firstName = 'Hiten';
+//  const lastName  = 'Pankhania';
+//  const middleName = 'Dinesh';
+//  const birthYear = 1999;
+//  const presentYear =2023;
+//  const New = `I am ${firstName} ${middleName} ${lastName} Age ${presentYear - birthYear} `
+//  console.log(New)
+
+// console.log(`Hello I am Hiten Pankhania Age 24 Currently Learining Javascript `)
+
+// console.log(`Hello
+//   You
+//    There `)
+
+// console.log('Hello \n\ You \n\ There')  //Note NOT RECOM
+
+// STOP STOP STOP STOP 
+
+// const age =24;
+// const isoldEnough = age >= 20
+// if(isoldEnough) {
+//     console.log('Is Leagle To Get Driving License')
+// }
+
+
+// const age = 15;
+
+// if(age=>18) {
+//     console.log('Can get driving license')
+// }else{
+//     const yearsLeft = 18 -age;
+//     console.log(`Cannot get driving license ${yearsLeft} Year:)`)
+// }
  
 
+//Type Conversion
+// const inputYear = '1991';
+// console.log(Number(inputYear) ,inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(String(22));//String will be always showned in White Colour In console
+
+
+// //Type Coercion
+// console.log('I am ' + 24 +' ' + 'Years old ');// This is shown as a String 
+// console.log('25' - '10' - 34)//In this case javascript in bacground will convert a string into a Number
+// console.log('22' * '22');//Multiplaction
+// console.log('10' / '10');//Divide
+// console.log ('200' * '18' / 100) //This is use to calculate %
+
+// let n ='1' + 1;
+// n + n - 1
+// console.log(n)
+
+//Truthly And Value
+console.log(Boolean(0)); //False
+console.log(Boolean(undefined)); //False
+console.log(Boolean('Hiten')); //True
+console.log(Boolean({})); //True
+console.log(Boolean('')); //False
 
