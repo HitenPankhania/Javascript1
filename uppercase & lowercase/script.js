4d74be52f68c418a2c42b9a4dcf33aa8199b789f
hitenpankhania <pankhaniahiten4@gmail.com>
2023-08-20 23:12:27 +0530

    okie

--- uppercase & lowercase/script.js
+++ uppercase & lowercase/script.js
@@ -3,10 +3,23 @@
 // name = name.toLowerCase();
 
 
-//Create a var that stores that save the name that user enters via prompt
+// 1 Create a var that stores that save the name that user enters via prompt
+
 var name =prompt("What is your name?");
 
-//Capatlise the first lletter of their name
+// 2 Capatlise the first letter of their Name
+var firstChar = name.slice(0.1)
+
+// A isolate the chara
+var upperCaseFirstChar = firstChar.toUpperCase()
+
+// B Turn the first case to upperCase
+var restOfName = name.slice(1.6)
+
+// C Isolate the rest of the name 
+
+// D concactenate the first chara to the rest of the chara
 
 
-//We use the capatalised version of their namee to greet the using an alert
+
+// 3 We use the capatalised version of their namee to greet the using an alert
