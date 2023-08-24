1ebb22ad9ecc56e2156c3ed646222926276f14a9
Hiten Pankhania <pankhaniahiten4@gmail.com>
2023-08-24 15:21:48 +0530

    okie

--- Dogs to Human Age/script.js
+++ Dogs to Human Age/script.js
@@ -1,3 +1,7 @@
-var dogAge = prompt (" How ols is the dog");
+var dogAge = prompt (" How old is the dog");
 var humanAge = ((dogAge - 2) * 4) + 2;
-alert("Your Dog is" + humanAge + "Year old in Hhumann age");
+alert("Your Dog is" + humanAge + "Year old in Human age");
+ 
+
+
+alert("Hello")
---
+++ Functions/script.js
@@ -0,0 +1,9 @@
+function getMilk(){
+alert("moveRight");
+alert("moveLeft");
+alert("moveUp");
+alert("moveDown");
+alert("Hello");
+}                  // Curley Brackets does end with semicolum and thats how javascript works
+
+console.log(getMilk);
