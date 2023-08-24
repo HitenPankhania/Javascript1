fddbd5283ba919a9cd25506e36ec608fe5b55a3b
Hiten Pankhania <pankhaniahiten4@gmail.com>
2023-08-24 16:46:58 +0530

    okie

---
+++ Small Game using Function/script.js
@@ -0,0 +1,88 @@
+// /**
+//  * Welcome to the Stanford Karel IDE.
+//  * This is a free space for you to 
+//  * write any Karel program you want.
+//  **/
+// function main(){
+//     //your code here
+//     helloCircle();
+//  }
+ 
+ 
+//  function helloCircle(){
+//     move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//     move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+//     move();
+//     turnRight();
+//      move();
+//     turnLeft();
+
+//  }
+
+
+
+//Parameters And Arguments
+
+function getMilk(){
+   
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("buy + bottles + bottles of milk ");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+
+
+    getMilk(12);
+
+
+}
